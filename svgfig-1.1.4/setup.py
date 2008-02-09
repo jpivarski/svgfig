0880297cfe1bb5378f9b94d2661bdbf94face26a
jim.belk <jim.belk@4019295b-0d44-0410-b858-7b557ddbaab2>
2008-02-09 08:33:03 +0000

    Copied current code into subfolder svgfig-1.1.4.

---
+++ svgfig-1.1.4/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+setup(name="SVGFig",
+      version="1.1.2",
+      description="SVGFig: Quantitative drawing in Python and SVG",
+      author="Jim Pivarski",
+      author_email="jpivarski@gmail.com",
+      url="http://code.google.com/p/svgfig/",
+      py_modules=["svgfig"],
+     )
