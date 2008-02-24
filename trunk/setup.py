b62e337c8cbbd8d91c51cb6a41f68c1272911e55
jpivarski <jpivarski@4019295b-0d44-0410-b858-7b557ddbaab2>
2008-02-24 12:56:26 +0000

    test multi-file structure and extension module on Windows

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
+import os
 
 setup(name="SVGFig",
-      version="1.1.2",
+      version="2.0.0",
       description="SVGFig: Quantitative drawing in Python and SVG",
       author="Jim Pivarski",
       author_email="jpivarski@gmail.com",
       url="http://code.google.com/p/svgfig/",
-      py_modules=["svgfig"],
+      py_modules=[os.path.join("svgfig", "__init__"),
+                  os.path.join("svgfig", "svg"),
+                  os.path.join("svgfig", "trans"),
+                  os.path.join("svgfig", "fig"),
+                  ],
+      ext_modules=[Extension(os.path.join("svgfig", "_compiled"), [os.path.join("svgfig", "_compiled.c")], {}),
+                   ],
      )
