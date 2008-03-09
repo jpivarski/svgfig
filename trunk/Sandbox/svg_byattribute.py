0360829f4dc19cacf81607f6fbbfafc695a738fc
jpivarski <jpivarski@4019295b-0d44-0410-b858-7b557ddbaab2>
2008-03-09 01:03:59 +0000

    rejected SVG attribute access via Python attributes

---
+++ trunk/Sandbox/svg_byattribute.py
@@ -0,0 +1,22 @@
+#########################################################################
+# Not clear that we want this
+#########################################################################
+# 
+#   def __getattr__(self, name):
+#     return self.attrib[self._preprocess_attribname(name)]
+# 
+#   def __setattr__(self, name, value):
+#     if name in self.__dict__:
+#       self.__dict__[name] = value
+#     else:
+#       self.attrib[self._preprocess_attribname(name)] = value
+# 
+#   def __delattr__(self, name):
+#     if name not in self.__dict__:
+#       del self.attrib[self._preprocess_attribname(name)]
+# 
+#   def __str__(self): return repr(self)
+# 
+#   def __iter__(self): return iter(self.children)
+#########################################################################
+
