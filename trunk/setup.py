8679945adb7d1a47295b9c773df0265e05d7b939
tw-public@gmx.de <tw-public@gmx.de@4019295b-0d44-0410-b858-7b557ddbaab2>
2008-12-03 17:45:34 +0000

    converted CRLF line endings to LF, reindented with python's Tools/scripts/reindent.py, no manual changes
    CRLF conversion was done with vim and :%s/\r$//g
    After that, reindent was used - quoting reindent's docstring:
    
    Change Python (.py) files to use 4-space indents and no hard tab characters.
    Also trim excess spaces and tabs from ends of lines, and remove empty lines
    at the end of files.  Also ensure the last line ends with a newline.

--- trunk/setup.py
+++ trunk/setup.py
@@ -12,30 +12,30 @@ extension_features = {
   }
 
 class my_build_ext(build_ext):
-  def build_extension(self, extension):
-    try:
-      build_ext.build_extension(self, extension)
-    except:
-      for ext, feat in extension_features.items():
-        if ext in extension.name and feat is None:
-          raise Exception, "Failed to compile %s" % ext
+    def build_extension(self, extension):
+        try:
+            build_ext.build_extension(self, extension)
+        except:
+            for ext, feat in extension_features.items():
+                if ext in extension.name and feat is None:
+                    raise Exception, "Failed to compile %s" % ext
 
-      print "************************************************************************************************"
-      print ""
-      print "Note: couldn't compile \"%s\", so you will be unable to use this feature:" % extension.name
-      print ""
-      for ext, feat in extension_features.items():
-        if ext in extension.name:
-          print feat
-          break
-      print ""
-      print "************************************************************************************************"
+            print "************************************************************************************************"
+            print ""
+            print "Note: couldn't compile \"%s\", so you will be unable to use this feature:" % extension.name
+            print ""
+            for ext, feat in extension_features.items():
+                if ext in extension.name:
+                    print feat
+                    break
+            print ""
+            print "************************************************************************************************"
 
 curve_extension = Extension(os.path.join("svgfig", "_curve"), [os.path.join("svgfig", "_curve.c")], {})
 
 def viewer_pkgconfig():
-  def drop_whitespace(word): return word != "\n" and word != ""
-  return filter(drop_whitespace, os.popen("pkg-config --cflags --libs gtk+-2.0 gthread-2.0").read().split(" "))
+    def drop_whitespace(word): return word != "\n" and word != ""
+    return filter(drop_whitespace, os.popen("pkg-config --cflags --libs gtk+-2.0 gthread-2.0").read().split(" "))
 
 viewer_extension = Extension(os.path.join("svgfig", "_viewer"),
                              [os.path.join("svgfig", "_viewer.c")], {},
--- trunk/svgfig/defaults.py
+++ trunk/svgfig/defaults.py
@@ -4,23 +4,23 @@ import pathdata
 version = "SVGFig 2.0.0alpha2"
 version_info = (2, 0, 0, "alpha2")
 
-############################### default filenames 
+############################### default filenames
 
 class VersionWarning(UserWarning): pass
 warnings.filterwarnings("default", category=VersionWarning)
 
 if re.search("windows", platform.system(), re.I):
-  try:
-    import _winreg
-    directory = _winreg.QueryValueEx(_winreg.OpenKey(_winreg.HKEY_CURRENT_USER,
-                r"Software\Microsoft\Windows\Current Version\Explorer\Shell Folders"), "Desktop")[0]
-  except:
-    directory = os.path.expanduser("~") + os.sep + "Desktop"
+    try:
+        import _winreg
+        directory = _winreg.QueryValueEx(_winreg.OpenKey(_winreg.HKEY_CURRENT_USER,
+                    r"Software\Microsoft\Windows\Current Version\Explorer\Shell Folders"), "Desktop")[0]
+    except:
+        directory = os.path.expanduser("~") + os.sep + "Desktop"
 
 def _expand_fileName(fileName):
-  if re.search("windows", platform.system(), re.I) and not os.path.isabs(fileName):
-    fileName = defaults.directory + os.sep + fileName
-  return fileName
+    if re.search("windows", platform.system(), re.I) and not os.path.isabs(fileName):
+        fileName = defaults.directory + os.sep + fileName
+    return fileName
 
 ############################### Defaults for each SVG element type
 
@@ -42,24 +42,24 @@ signature_circle = ["cx", "cy", "r", "stroke", "fill"]
 require_circle = ["cx", "cy", "r"]
 defaults_circle = {"stroke": "black", "fill": "none"}
 def tonumber_circle(svg):
-  svg.cx, svg.cy, svg.r = tonumber(svg.cx), tonumber(svg.cy), tonumber(svg.r)
+    svg.cx, svg.cy, svg.r = tonumber(svg.cx), tonumber(svg.cy), tonumber(svg.r)
 
 def transform_circle(trans, svg):
-  if isnumber(svg.cx) and isnumber(svg.cy):
-    x1, y1 = trans(svg.cx, svg.cy)
-    if isnumber(svg.r):
-      x2, y2 = trans(svg.cx + svg.r, svg.cy)
-      svg.r = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
-    svg.cx, svg.cy = x1, y1
+    if isnumber(svg.cx) and isnumber(svg.cy):
+        x1, y1 = trans(svg.cx, svg.cy)
+        if isnumber(svg.r):
+            x2, y2 = trans(svg.cx + svg.r, svg.cy)
+            svg.r = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+        svg.cx, svg.cy = x1, y1
 
 def bbox_circle(svg):
-  if isnumber(svg.cx) and isnumber(svg.cy):
-    if isnumber(svg.r):
-      return BBox(svg.cx - svg.r, svg.cx + svg.r, svg.cy - svg.r, svg.cy + svg.r)
+    if isnumber(svg.cx) and isnumber(svg.cy):
+        if isnumber(svg.r):
+            return BBox(svg.cx - svg.r, svg.cx + svg.r, svg.cy - svg.r, svg.cy + svg.r)
+        else:
+            return BBox(svg.cx, svg.cx, svg.cy, svg.cy)
     else:
-      return BBox(svg.cx, svg.cx, svg.cy, svg.cy)
-  else:
-    return BBox(None, None, None, None)
+        return BBox(None, None, None, None)
 
 ##### clipPath
 ##### color-profile
@@ -115,23 +115,23 @@ require_line = ["x1", "y1", "x2", "y2"]
 defaults_line = {"stroke": "black"}
 
 def tonumber_line(svg):
-  svg.x1, svg.y1, svg.x2, svg.y2 = tonumber(svg.x1), tonumber(svg.y1), tonumber(svg.x2), tonumber(svg.y2)
+    svg.x1, svg.y1, svg.x2, svg.y2 = tonumber(svg.x1), tonumber(svg.y1), tonumber(svg.x2), tonumber(svg.y2)
 
 def transform_line(trans, svg):
-  if isnumber(svg.x1) and isnumber(svg.y1):
-    svg.x1, svg.y1 = trans(svg.x1, svg.y1)
+    if isnumber(svg.x1) and isnumber(svg.y1):
+        svg.x1, svg.y1 = trans(svg.x1, svg.y1)
 
-  if isnumber(svg.x2) and isnumber(svg.y2):
-    svg.x2, svg.y2 = trans(svg.x2, svg.y2)
+    if isnumber(svg.x2) and isnumber(svg.y2):
+        svg.x2, svg.y2 = trans(svg.x2, svg.y2)
 
 def bbox_line(svg):
-  isnumber1 = (isnumber(svg.x1) and isnumber(svg.y1))
-  isnumber2 = (isnumber(svg.x2) and isnumber(svg.y2))
+    isnumber1 = (isnumber(svg.x1) and isnumber(svg.y1))
+    isnumber2 = (isnumber(svg.x2) and isnumber(svg.y2))
 
-  if isnumber1 and isnumber2: return BBox(svg.x1, svg.x2, svg.y1, svg.y2)
-  elif isnumber1 and not isnumber2: return BBox(svg.x1, svg.x1, svg.y1, svg.y1)
-  elif not isnumber1 and isnumber2: return BBox(svg.x2, svg.x2, svg.y2, svg.y2)
-  else: return BBox(None, None, None, None)
+    if isnumber1 and isnumber2: return BBox(svg.x1, svg.x2, svg.y1, svg.y2)
+    elif isnumber1 and not isnumber2: return BBox(svg.x1, svg.x1, svg.y1, svg.y1)
+    elif not isnumber1 and isnumber2: return BBox(svg.x2, svg.x2, svg.y2, svg.y2)
+    else: return BBox(None, None, None, None)
 
 ##### linearGradient
 ##### marker
@@ -147,14 +147,14 @@ require_path = []
 defaults_path = {"d": [], "stroke": "black", "fill": "none"}
 
 def tonumber_path(svg):
-  svg.d = pathdata.parse(svg.d)
+    svg.d = pathdata.parse(svg.d)
 
 def transform_path(trans, svg):
-  svg.d = pathdata.transform(trans, svg.d)
+    svg.d = pathdata.transform(trans, svg.d)
 
 def bbox_path(svg):
-  return pathdata.bbox(pathdata.parse(svg.d))
-  
+    return pathdata.bbox(pathdata.parse(svg.d))
+
 ##### pattern
 ##### polygon
 ##### polyline
@@ -165,23 +165,23 @@ require_rect = ["x", "y", "width", "height"]
 defaults_rect = {"stroke": "black", "fill": "none"}
 
 def transform_rect(trans, svg):
-  if isnumber(svg.x) and isnumber(svg.y):
-    if isnumber(svg.width) and isnumber(svg.height):
-      x1, y1 = trans(svg.x, svg.y)
-      x2, y2 = trans(svg.x + svg.width, svg.y + svg.height)
-      svg.x, svg.y = x1, y1
-      svg.width, svg.height = x2 - x1, y2 - y1
-    else:
-      svg.x, svg.y = trans(svg.x, svg.y)
+    if isnumber(svg.x) and isnumber(svg.y):
+        if isnumber(svg.width) and isnumber(svg.height):
+            x1, y1 = trans(svg.x, svg.y)
+            x2, y2 = trans(svg.x + svg.width, svg.y + svg.height)
+            svg.x, svg.y = x1, y1
+            svg.width, svg.height = x2 - x1, y2 - y1
+        else:
+            svg.x, svg.y = trans(svg.x, svg.y)
 
 def bbox_rect(svg):
-  if isnumber(svg.x) and isnumber(svg.y):
-    if isnumber(svg.width) and isnumber(svg.height):
-      return BBox(svg.x, svg.x + svg.width, svg.y, svg.y + svg.height)
+    if isnumber(svg.x) and isnumber(svg.y):
+        if isnumber(svg.width) and isnumber(svg.height):
+            return BBox(svg.x, svg.x + svg.width, svg.y, svg.y + svg.height)
+        else:
+            return BBox(svg.x, svg.x, svg.y, svg.y)
     else:
-      return BBox(svg.x, svg.x, svg.y, svg.y)
-  else:
-    return BBox(None, None, None, None)
+        return BBox(None, None, None, None)
 
 ##### script
 ##### set
@@ -197,11 +197,11 @@ defaults_svg = {"width": 400, "height": 400, "viewBox": (0, 0, 100, 100),
                 }
 
 def tonumber_svg(svg):
-  svg.width = tonumber(svg.width)
-  svg.height = tonumber(svg.height)
-  svg.viewBox = tonumberlist(svg.viewBox)
-  svg["style"] = tostringmap(svg["style"])
-  svg["font-family"] = tostringlist(svg["font-family"])
+    svg.width = tonumber(svg.width)
+    svg.height = tonumber(svg.height)
+    svg.viewBox = tonumberlist(svg.viewBox)
+    svg["style"] = tostringmap(svg["style"])
+    svg["font-family"] = tostringlist(svg["font-family"])
 
 ##### switch
 ##### symbol
@@ -213,18 +213,18 @@ require_text = ["x", "y"]
 defaults_text = {"stroke": "none", "fill": "black"}
 
 def tonumber_text(svg):
-  svg.x = tonumber(svg.x)
-  svg.y = tonumber(svg.y)
+    svg.x = tonumber(svg.x)
+    svg.y = tonumber(svg.y)
 
 def transform_text(trans, svg):
-  if isnumber(svg.x) and isnumber(svg.y):
-    svg.x, svg.y = trans(svg.x, svg.y)
+    if isnumber(svg.x) and isnumber(svg.y):
+        svg.x, svg.y = trans(svg.x, svg.y)
 
 def bbox_text(svg):
-  if isnumber(svg.x) and isnumber(svg.y):
-    return BBox(svg.x, svg.x, svg.y, svg.y) # how to calculate text size???
-  else:
-    return BBox(None, None, None, None)
+    if isnumber(svg.x) and isnumber(svg.y):
+        return BBox(svg.x, svg.x, svg.y, svg.y) # how to calculate text size???
+    else:
+        return BBox(None, None, None, None)
 
 ##### textPath
 ##### title
@@ -237,18 +237,18 @@ signature_use = ["x", "y", "xlink:href"]
 require_use = ["x", "y", "xlink:href"]
 
 def tonumber_use(svg):
-  svg.x = tonumber(svg.x)
-  svg.y = tonumber(svg.y)
+    svg.x = tonumber(svg.x)
+    svg.y = tonumber(svg.y)
 
 def transform_use(trans, svg):
-  if isnumber(svg.x) and isnumber(svg.y):
-    svg.x, svg.y = trans(svg.x, svg.y)
+    if isnumber(svg.x) and isnumber(svg.y):
+        svg.x, svg.y = trans(svg.x, svg.y)
 
 def bbox_use(svg):
-  if isnumber(svg.x) and isnumber(svg.y):
-    return BBox(svg.x, svg.x, svg.y, svg.y)
-  else:
-    return BBox(None, None, None, None)
+    if isnumber(svg.x) and isnumber(svg.y):
+        return BBox(svg.x, svg.x, svg.y, svg.y)
+    else:
+        return BBox(None, None, None, None)
 
 ##### view
 ##### vkern
@@ -256,75 +256,75 @@ def bbox_use(svg):
 ############################### utility functions for default actions
 
 def tonumber(x):
-  try:
-    return float(x)
-  except ValueError:
-    return x
+    try:
+        return float(x)
+    except ValueError:
+        return x
 
 def tonumberlist(x):
-  if isinstance(x, basestring):
-    try:
-      return tuple(map(float, re.split("[, \t]+", x)))
-    except ValueError:
-      return x
-  return x
+    if isinstance(x, basestring):
+        try:
+            return tuple(map(float, re.split("[, \t]+", x)))
+        except ValueError:
+            return x
+    return x
 
 def tostringlist(x):
-  if isinstance(x, basestring):
-    return re.split("[, \t]+", x)
-  return x
+    if isinstance(x, basestring):
+        return re.split("[, \t]+", x)
+    return x
 
 def tostringmap(x):
-  if isinstance(x, basestring):
-    try:
-      return dict(map(lambda word: word.split(":"), re.split("[; \t]+", x)))
-    except ValueError:
-      return x
-  else:
-    return x
+    if isinstance(x, basestring):
+        try:
+            return dict(map(lambda word: word.split(":"), re.split("[; \t]+", x)))
+        except ValueError:
+            return x
+    else:
+        return x
 
 def isnumber(x): return isinstance(x, (int, long, float))
 
 ############################### BBox class
 
 class BBox:
-  def __init__(self, xmin, xmax, ymin, ymax):
-    self.xmin, self.xmax, self.ymin, self.ymax = xmin, xmax, ymin, ymax
+    def __init__(self, xmin, xmax, ymin, ymax):
+        self.xmin, self.xmax, self.ymin, self.ymax = xmin, xmax, ymin, ymax
 
-  def __repr__(self):
-    return "<BBox xmin=%g xmax=%g ymin=%g ymax=%g>" % (self.xmin, self.xmax, self.ymin, self.ymax)
+    def __repr__(self):
+        return "<BBox xmin=%g xmax=%g ymin=%g ymax=%g>" % (self.xmin, self.xmax, self.ymin, self.ymax)
 
-  def insert(self, x, y):
-    if self.xmin == None or x < self.xmin: self.xmin = x
-    if self.ymin == None or y < self.ymin: self.ymin = y
-    if self.xmax == None or x > self.xmax: self.xmax = x
-    if self.ymax == None or y > self.ymax: self.ymax = y
+    def insert(self, x, y):
+        if self.xmin == None or x < self.xmin: self.xmin = x
+        if self.ymin == None or y < self.ymin: self.ymin = y
+        if self.xmax == None or x > self.xmax: self.xmax = x
+        if self.ymax == None or y > self.ymax: self.ymax = y
 
-  def __add__(self, other):
-    output = BBox(self.xmin, self.xmax, self.ymin, self.ymax)
-    output += other
-    return output
+    def __add__(self, other):
+        output = BBox(self.xmin, self.xmax, self.ymin, self.ymax)
+        output += other
+        return output
 
-  def __iadd__(self, other):
-    if self.xmin is None: self.xmin = other.xmin
-    elif other.xmin is None: pass
-    else: self.xmin = min(self.xmin, other.xmin)
+    def __iadd__(self, other):
+        if self.xmin is None: self.xmin = other.xmin
+        elif other.xmin is None: pass
+        else: self.xmin = min(self.xmin, other.xmin)
 
-    if self.xmax is None: self.xmax = other.xmax
-    elif other.xmax is None: pass
-    else: self.xmax = max(self.xmax, other.xmax)
+        if self.xmax is None: self.xmax = other.xmax
+        elif other.xmax is None: pass
+        else: self.xmax = max(self.xmax, other.xmax)
 
-    if self.ymin is None: self.ymin = other.ymin
-    elif other.ymin is None: pass
-    else: self.ymin = min(self.ymin, other.ymin)
+        if self.ymin is None: self.ymin = other.ymin
+        elif other.ymin is None: pass
+        else: self.ymin = min(self.ymin, other.ymin)
 
-    if self.ymax is None: self.ymax = other.ymax
-    elif other.ymax is None: pass
-    else: self.ymax = max(self.ymax, other.ymax)
+        if self.ymax is None: self.ymax = other.ymax
+        elif other.ymax is None: pass
+        else: self.ymax = max(self.ymax, other.ymax)
 
-    return self
+        return self
 
-  def __eq__(self, other):
-    return self.xmin == other.xmin and self.xmax == other.xmax and self.ymin == other.ymin and self.ymax == other.ymax
+    def __eq__(self, other):
+        return self.xmin == other.xmin and self.xmax == other.xmax and self.ymin == other.ymin and self.ymax == other.ymax
 
-  def __ne__(self, other): return not (self == other)
+    def __ne__(self, other): return not (self == other)
