07ecabbbe88f7cfd264be018e00c00694ee3f2d2
explodingmind@gmail.com <explodingmind@gmail.com@4019295b-0d44-0410-b858-7b557ddbaab2>
2011-08-03 14:56:34 +0000

    added pure-python viewer and associate mods to svg.py, added ability for poly to take a zipped list, added rx/ry to defaults for 'rect', allowing them to be accessed with dot notation

--- trunk/svgfig/pathdata.py
+++ trunk/svgfig/pathdata.py
@@ -10,7 +10,8 @@ def poly(*data, **kwds):
         del kwds["loop"]
     if len(kwds) > 0:
         raise TypeError, errstring
-
+	if len(data) = 1:
+		data = data[0]
     try:
         output = []
         for x, y in data:
