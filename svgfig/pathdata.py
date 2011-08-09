bb0fa454d77f7046d5497f7959b593b23e5af8ae
explodingmind@gmail.com <explodingmind@gmail.com@4019295b-0d44-0410-b858-7b557ddbaab2>
2011-08-09 15:56:30 +0000

    added destroy callback in _viewer.py, added ability to pass list of tuples to pathdata.py

--- svgfig/pathdata.py
+++ svgfig/pathdata.py
@@ -10,8 +10,10 @@ def poly(*data, **kwds):
         del kwds["loop"]
     if len(kwds) > 0:
         raise TypeError, errstring
-	if len(data) == 1:
-		data = data[0]
+
+    if len(data) <= 1:
+        data = data[0]
+
     try:
         output = []
         for x, y in data:
