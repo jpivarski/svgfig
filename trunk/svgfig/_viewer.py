0f634e2e365b5bbf2db343941008642e916af3c9
explodingmind@gmail.com <explodingmind@gmail.com@4019295b-0d44-0410-b858-7b557ddbaab2>
2011-08-09 15:56:30 +0000

    added destroy callback in _viewer.py, added ability to pass list of tuples to pathdata.py

--- trunk/svgfig/_viewer.py
+++ trunk/svgfig/_viewer.py
@@ -6,17 +6,21 @@ import gtk
 class View:
 	def __init__(self):
 		self.string = """<svg width="800" height="600"></svg>"""
+		self.svg = rsvg.Handle(data=self.string)
 		self.win = gtk.Window()
 		self.da = gtk.DrawingArea()
 		self.win.add(self.da)
 		self.da.set_size_request(800, 600)
 		self.da.connect("expose-event", self.expose_cairo)
+		self.win.connect("destroy", self.destroy)
 		self.win.show_all()
 		self.win.present()
 	def expose_cairo(self, win, event):
-		svg = rsvg.Handle(data=self.string)
+		self.svg = rsvg.Handle(data=self.string)
 		cr = self.da.window.cairo_create()
-		svg.render_cairo(cr)
+		self.svg.render_cairo(cr)
+	def destroy(self, widget, data=None):
+		gtk.main_quit()
 	def renderSVG(self, text):
 		x, y, w, h = self.win.allocation
 		self.da.window.invalidate_rect((0,0,w,h), False)
